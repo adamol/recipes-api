da215734cfbc5ab38ec8d2d3695ebeb01c841e6b
adamol <adamol1992@gmail.com>
2017-09-02 10:03:06 +0200

    Add mongoose and refactor intial routes to use db

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 recipes = [
     {
         'name': 'Pasta Bolognese',
+        'slug': 'pasta-bolognese',
         'ingredients': [
             '2 tbsp Sun Dried Tomato Paste',
             '1 small SuperValu Carrot chopped',
@@ -22,6 +23,7 @@ recipes = [
     },
     {
         'name': 'Slow Cooker Chicken Thighs',
+        'slug': 'slow-cooker-chicken-thighs',
         'ingredients': [
             '1 tbsp. extra-virgin olive oil',
             '6 bone-in or boneless chicken thighs',
@@ -41,17 +43,65 @@ recipes = [
     }
 ]
 
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/recipes_db')
+
+var Schema = mongoose.Schema;
+
+var RecipeSchema = new Schema({
+    id: Schema.ObjectId,
+    slug: String,
+    name: String,
+    ingredients: {type: Array, "default": []},
+    description: String,
+}, {timestamps: true})
+
+var Recipe = mongoose.model('Recipe', RecipeSchema);
+
+// mongoose.connection.dropDatabase();
+
+// var recipeA = new Recipe(recipes[0]);
+// recipeA.save(function(err) {
+//     if(err) console.log(err);
+// });
+//
+// var recipeB = new Recipe(recipes[1]);
+// recipeB.save(function(err) {
+//     if(err) console.log(err);
+// });
 
 app.get('/', function(req, res) {
     res.send('Hello World')
 })
 
 app.get('/recipes', function(req, res) {
-    res.send(recipes)
+    Recipe.find(function(err, recipes) {
+        if (err) res.send(err);
+
+        res.json(recipes)
+    });
 })
 
-app.get('/recipes/:recipeId', function(req, res) {
-    res.send(recipes[req.params.recipeId])
+app.get('/recipes/:slug', function(req, res) {
+    Recipe.find({slug: req.params.slug}, function(err, recipes) {
+        if (err) res.send(err);
+
+        res.json(recipes)
+    });
+})
+
+app.post('/recipes', function(req, res) {
+    var recipe = new Recipe();
+
+    recipe.name = req.body.name;
+    recipe.ingredients = req.body.ingredients;
+    recipe.description = req.body.description;
+
+    recipe.save(function(err) {
+        if (err) res.send(err);
+
+        res.json({message: 'Recipe inserted into database'});
+    });
 })
 
 app.listen(3000, function() {
