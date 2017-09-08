c50d600cdf47562c8c3549b4baa66da00c2d7d89
adamol <adamol1992@gmail.com>
2017-09-08 23:25:13 +0200

    Implement JWT authentication

--- index.js
+++ index.js
@@ -27,7 +27,84 @@ app.use(morgan('dev'))
 app.get('/', function(req, res) {
     res.send('Hello World')
 })
+// ==================================
+// ===== auth routes ================
+// ==================================
+app.post('/register', function(req, res) {
+    var user = new User({
+        name: req.body.name,
+        password: req.body.password,
+    });
 
+    user.save(function(err) {
+        if (err) throw err;
+
+        console.log('New user record saved to database');
+
+        res.json({ success: true });
+    });
+});
+
+app.post('/authenticate', function(req, res) {
+    User.findOne({ name: req.body.name }, function(err, user) {
+        if (err) throw err;
+
+        if (! user) {
+            res.json({
+                success: false,
+                message: 'Authentication failed, user not found.'
+            });
+        }
+
+        if (user.password != req.body.password) {
+            res.json({
+                success: false,
+                message: 'Authentication failed, incorrect password.'
+            });
+        }
+
+        var token = jwt.sign({ user: user }, app.get('supersecret'));
+
+        res.json({ success: true, message: token });
+    });
+});
+
+var apiRoutes = express.Router();
+
+apiRoutes.use(function(req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    if (! token) {
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
+
+    jwt.verify(token, app.get('supersecret'), function(err, decoded) {
+        if (err) {
+            return res.json({
+                success: false,
+                message: 'Failed to authenticate token.'
+            });
+        }
+
+        req.decoded = decoded;
+        next();
+    });
+});
+
+app.use('/api', apiRoutes);
+
+app.get('/api/users', function(req, res) {
+    User.find({}, function(err, users) {
+        res.json(users);
+    });
+});
+
+// ==================================
+// ===== recipe routes ==============
+// ==================================
 app.get('/recipes', function(req, res) {
     Recipe.find(function(err, recipes) {
         if (err) res.send(err);
