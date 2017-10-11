53143342c90b3d2496d1a449432d0d11e99de9ee
adamol <adamol1992@gmail.com>
2017-10-11 19:42:07 +0200

    Delete unused models folder

--- app/models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose   = require('mongoose')
-var Schema   = mongoose.Schema;
-
-module.exports = mongoose.model('User', new Schema({
-    id: Schema.ObjectId,
-    name: String,
-    password: String,
-}, {timestamps: true}));
-
