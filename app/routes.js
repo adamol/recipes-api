85b3d05faa428be5863e507a22ada6203d6c57fd
adamol <adamol1992@gmail.com>
2017-10-11 20:14:12 +0200

    Extract routes from index.js

---
+++ app/routes.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router  = express.Router();
+
+router.get('/', function(req, res) {
+    res.send('Hello World')
+})
+
+var authController = require('./controllers/auth-controller');
+router.post('/register', authController.register);
+router.post('/authenticate', authController.authenticate);
+
+var recipeController = require('./controllers/recipe-controller');
+router.get('/recipes', recipeController.index);
+router.get('/recipes/:slug', recipeController.show);
+router.post('/recipes', recipeController.store);
+
+var mw = require('./middleware');
+router.use('/api', mw.jwt);
+
+var userController = require('./controllers/user-controller');
+router.get('/api/users', userController.index);
+
+module.exports = router;
--- index.js
+++ index.js
@@ -4,37 +4,16 @@ var bodyParser = require('body-parser')
 var morgan     = require('morgan')
 var mongoose   = require('mongoose')
 var config = require('./config')
-
-// configuration
 var port = process.env.PORT || 3000;
+
 mongoose.connect(config.database)
 app.set('supersecret', config.secret);
 
-// set up bodyParser to read POST requests and URL params
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// use morgan to log requests
 app.use(morgan('dev'))
 
-app.get('/', function(req, res) {
-    res.send('Hello World')
-})
-
-var authController = require('./app/controllers/auth-controller');
-app.post('/register', authController.register);
-app.post('/authenticate', authController.authenticate);
-
-var recipeController = require('./app/controllers/recipe-controller');
-app.get('/recipes', recipeController.index);
-app.get('/recipes/:slug', recipeController.show);
-app.post('/recipes', recipeController.store);
-
-var mw = require('./app/middleware');
-app.use('/api', mw.jwt);
-
-var userController = require('./app/controllers/user-controller');
-app.get('/api/users', userController.index);
+app.use('/', require('./app/routes'));
 
 app.listen(port, function() {
     console.log('App listening on port ' + port)
